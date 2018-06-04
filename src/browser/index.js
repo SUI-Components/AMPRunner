e92925f82a6b915dee55202dbaa2c51565c1ad3c
Carlos Villuendas <carlosvillu@gmail.com>
2018-06-04 14:56:56 +0200

    feat(browser): create cleanProfileFolder function

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer')
+const rimraf = require('rimraf')
 const getChromePath = require('@browserless/aws-lambda-chrome')({
   path: '/tmp'
 })
@@ -6,16 +7,29 @@ const getChromePath = require('@browserless/aws-lambda-chrome')({
 const isLambda = !!process.env.LAMBDA_TASK_ROOT
 const getExecutablePath = async () => (isLambda ? getChromePath() : undefined)
 
-module.exports = async () =>
-  puppeteer.launch({
-    headless: isLambda,
-    executablePath: await getExecutablePath(),
-    ignoreHTTPSErrors: true,
-    args: [
-      '--disable-gpu',
-      '--single-process', // Currently wont work without this :-(
-      isLambda ? '--no-zygote' : undefined, // helps avoid zombies
-      '--no-sandbox',
-      '--hide-scrollbars'
-    ].filter(Boolean)
-  })
+module.exports = {
+  cleanProfileFolder: browser => {
+    if (!browser) {
+      return
+    }
+    const chromeSpawnArgs = browser.process().spawnargs
+    const [, pathProfile] = chromeSpawnArgs
+      .find(flag => flag.includes('--user-data-dir'))
+      .split('=')
+    rimraf.sync(pathProfile)
+    console.log('Delete profile folder:', pathProfile)
+  },
+  initBrowser: async () =>
+    puppeteer.launch({
+      headless: isLambda,
+      executablePath: await getExecutablePath(),
+      ignoreHTTPSErrors: true,
+      args: [
+        '--disable-gpu',
+        '--single-process', // Currently wont work without this :-(
+        isLambda ? '--no-zygote' : undefined, // helps avoid zombies
+        '--no-sandbox',
+        '--hide-scrollbars'
+      ].filter(Boolean)
+    })
+}
